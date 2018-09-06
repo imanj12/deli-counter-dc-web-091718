c5ceb7beeb21f3a056ca3d8fd9cbe1267742cacf
Iman Jawad <imanj12@gmail.com>
2018-09-06 15:38:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  line_spot = katz_deli.index(name) + 1
+  line_spot = katz_deli.length
   puts "Welcome, #{name}. You are number #{line_spot} in line."
 end
 
